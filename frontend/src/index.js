7d5db61d371654110d94219de1d1c85809a5800a
Thomas Ludlow <twjludlow@gmail.com>
2017-12-21 21:49:51 +0000

    meow

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,56 +1,8 @@
-/*
-  Import Dependencies
-*/
 import React from 'react';
-import {render} from 'react-dom';
-import {Provider} from 'react-redux';
-import {Router, Route, IndexRoute} from 'react-router'
-import 'babel-polyfill';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import registerServiceWorker from './registerServiceWorker';
 
-/* Import CSS */
-//eslint-disable-next-line
-import css from './styles/main.scss';
-
-/*
-  Import Components
-*/
-import App from './components/App';
-
-/*
-  Import Views
-*/
-import Home from "./views/Home";
-
-/* Import our data store */
-import store, {history} from './store';
-
-/*
-  Rendering
-  This is where we hook up the Store with our actual component and the router
-*/
-render(
-  <Provider store={store}>
-    { /* Tell the Router to use our enhanced history */ }
-    <Router history={history}>
-      <Route path="/" component={App}>
-        <IndexRoute component={Home} />
-      </Route>
-    </Router>
-  </Provider>,
-  document.getElementById('root')
-);
-
-/*
-render(
-  <Provider store={store}>
-    <Router history={history}>
-      <Route path="/" component={App}>
-        <IndexRoute component={PhotoGrid} />
-        <Route path="/view/:postId" component={Single}></Route>
-      </Route>
-    </Router>
-  </Provider>,
-  document.getElementById('root')
-);
-
-*/
+ReactDOM.render(<App />, document.getElementById('root'));
+registerServiceWorker();
