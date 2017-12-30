cfab253051eff652edc40d22d6af186172e2daa8
Thomas Ludlow <twjludlow@gmail.com>
2017-12-30 18:05:02 +0000

    Generalised error handling on Main.js component

--- frontend/src/components/Main.js
+++ frontend/src/components/Main.js
@@ -2,7 +2,17 @@ import React, {Component} from 'react';
 import {StripeProvider} from "react-stripe-elements";
 import axios from "axios";
 
+import Navbar from "./Navbar/Navbar";
+
 export default class Main extends Component {
+
+    constructor(props) {
+        super();
+        this.state = {
+            didCatch: false,
+            error: ""
+        };
+    }
 
     componentDidMount() {
         if (this.props.user.loggedIn) {
@@ -11,8 +21,25 @@ export default class Main extends Component {
         }
     }
 
+    componentDidCatch(error, info) {
+        this.setState({didCatch: true, error: error});
+        console.log("error caught", error);
+        console.log("info caught", info);
+    }
+
     //Main component that renders child components.
     render() {
+        if(this.state.didCatch) {
+            return (
+                <div className="base">
+                    <Navbar />
+                    <div className="container">
+                        <h3>There was an error in your program.</h3>
+                        <p>{this.state.error}</p>
+                    </div>
+                </div>
+            );
+        }
         return (
             <StripeProvider apiKey="pk_test_uRsCi66QQQhKPb5JRUoAR8hh">
                 <div>
